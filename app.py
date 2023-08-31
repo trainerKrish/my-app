06b3923882d6e4c4efb3d6586bc3cc4707fb4fb6
Krish <krish@company.com>
2023-08-31 07:09:47 +0530

    Added new routes

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ import flask
 
 
 app = flask.Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return flask.render_template("home.html")
+
+
+@app.route("/contact-us")
+def home():
+    return flask.render_template("contact-us.html")
+
